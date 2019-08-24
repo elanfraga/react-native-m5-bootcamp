df6e477ac72814bf9496c61663495de32a1a8163
Elan Fraga <elanfraga@gmail.com>
2019-08-24 16:49:07 -0300

    Buscando dados da API

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View } from 'react-native';
+import PropTypes from 'prop-types';
+import api from '../../services/api';
 
 // import { Container } from './styles';
 
-export default function Main({ navigation }) {
-  console.tron.log(navigation.getParam('user'));
+export default class User extends Component {
+  static navigationOptions = ({ navigation }) => ({
+    title: navigation.getParam('name'),
+  });
 
-  return <View />;
+  static propTypes = {
+    navigation: PropTypes.shape({
+      getParam: PropTypes.func,
+    }).isRequired,
+  };
+
+  state = {
+    stars: [],
+  };
+
+  async componentDidMount() {
+    const { navigation } = this.props;
+    const login = navigation.getParam('login');
+
+    const response = await api.get(`/users/${login}/starred`);
+
+    this.setState({ stars: response.data });
+  }
+
+  render() {
+    const { stars } = this.state;
+    return <View />;
+  }
 }
